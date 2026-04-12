2942052e92392935a2e99a8e845e2527e472239f
Marchi30847 <s30847@pjwstk.edu.pl>
2026-04-12 19:41:51 +0200

    chore(project): add initial directory structure of the project, settings and entry point

---
+++ src/piano_app/bootstrap/settings/__init__.py
@@ -0,0 +1,8 @@
+from .load import load_environment, load_settings
+from .models import Settings
+
+__all__ = (
+    "Settings",
+    "load_environment",
+    "load_settings",
+)
---
+++ src/piano_app/bootstrap/settings/models.py
@@ -0,0 +1,10 @@
+from typing import ClassVar
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config: ClassVar[SettingsConfigDict] = SettingsConfigDict(
+        extra="ignore",
+        case_sensitive=True,
+    )
